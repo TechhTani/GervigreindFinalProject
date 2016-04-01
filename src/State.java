611a6c573689b74f99c57f02bf89a077420125e4
krissi92 <jayflug@gmail.com>
2016-04-01 17:44:43 +0000

    meh

--- src/State.java
+++ src/State.java
@@ -1,5 +1,12 @@
 /**
  * Created by Kristján on 1.4.2016.
  */
+import static org.chocosolver.solver.search.strategy.ISF.*;
+
 public class State {
+    Object variable;
+    public State(String board){
+
+    }
+
 }
