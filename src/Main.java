55b421bdf67e49c2ac6a8bc78eead19aa535f2b1
Katur <grimurk@gmail.com>
2016-04-01 17:06:18 +0000

    Read in a puzzle from text file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 /**
- * Created by Kristján on 1.4.2016.
+ * Created by Kristjan on 1.4.2016.
  */
 public class Main {
-    int hello = 123;
+	
+	/**
+	 * main procedure
+	 */
+	public static void main(String[] args) throws Exception {
+	    try(BufferedReader br = new BufferedReader(new FileReader("easy-puzzle.txt"))) {
+	        StringBuilder sb = new StringBuilder();
+	        String line = br.readLine();
+
+	        while (line != null) {
+	            sb.append(line);
+	            sb.append(System.lineSeparator());
+	            line = br.readLine();
+	        }
+	        String everything = sb.toString();
+	        System.out.print(everything);
+	    }
+	}
+    
 }
