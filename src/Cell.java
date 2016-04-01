1590fb1b6ae62bfa713f475a55aad34ed1048f33
Natan <natan_ola@hotmail.com>
2016-04-01 18:02:58 +0000

    Added cell class

---
+++ src/Cell.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Natan on 01.04.2016.
+ */
+public class Cell {
+
+    public int value;
+    public int[] possibleValues;
+
+    public Cell(int value, int[] possibleValues){
+        this.value = value;
+        this.possibleValues = possibleValues;
+    }
+}
