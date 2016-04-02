dbe2a5c77020fc34da7aa301b90d8f5a76e23f54
Solvi Hjaltason <solvihj@gmail.com>
2016-04-02 14:51:42 +0000

    asdf

--- src/Agent.java
+++ src/Agent.java
@@ -21,12 +21,9 @@ public class Agent {
 
     public void test(){
         Solver solver = new Solver("First problem");
+        
         IntVar[][] vs = VariableFactory.boundedMatrix("vs", 9, 9, 1, 9, solver);
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
-                vs[i][j] = Board.charAt(j+i);
-            }
-        }
+        
         solver.post(IntConstraintFactory.arithm(x, "+", y, "<", 5));
         // 8 // 4. Define the search strategy 9
         solver.set(IntStrategyFactory.lexico_LB(x, y));
