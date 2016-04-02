533325eebdbb8ba5071e1ab706a79ff156a82cac
Solvi Hjaltason <solvihj@gmail.com>
2016-04-02 15:59:18 +0000

    You wanna fkn go m8?

--- src/Agent.java
+++ src/Agent.java
@@ -3,12 +3,11 @@
  */
 import org.chocosolver.solver.Solver;
 import static org.chocosolver.solver.search.strategy.ISF.*;
+import org.chocosolver.util.tools.ArrayUtils;
 import org.chocosolver.solver.constraints.IntConstraintFactory;
 import org.chocosolver.solver.search.strategy.IntStrategyFactory;
-import org.chocosolver.solver.trace.Chatterbox;
 import org.chocosolver.solver.variables.IntVar;
 import org.chocosolver.solver.variables.VariableFactory;
-
 
 public class Agent {
     final int SIZE = 9;
@@ -43,11 +42,20 @@ public class Agent {
         for(int i = 0; i < 3; i++){
         	for(int j = 0; j < 3; j++){
         		for(int k = 0; k < 3; k++){
-        			boxing[i][j] = ripped[j][k];
+        			boxing[j+k*3][i] = ripped[k*3][i+j*3];
+        			boxing[j+k*3][i+3] = ripped[1+k*3][i+j*3];
+        			boxing[j+k*3][i+6] = ripped[2+k*3][i+j*3];
         		}
     		}
         }
+        
+        for(int i = 0; i < SIZE; i++){
+        	solver.post(IntConstraintFactory.alldifferent(ripped[i], "DEFAULT"));
+        	solver.post(IntConstraintFactory.alldifferent(colons[i], "DEFAULT"));
+        	solver.post(IntConstraintFactory.alldifferent(boxing[i], "DEFAULT"));
+        }
+        
+        solver.set(IntStrategyFactory.firstFail_InDomainMin(ArrayUtils.append(ripped)));
     }
-
 
 }
